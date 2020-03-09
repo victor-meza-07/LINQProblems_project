199735231f0c0da979f68f1b0fcbf7142a875b38
victor-meza-07 <victord.meza@gmail.com>
2020-03-09 16:54:22 -0500

    problem 3 solved

--- LINQProblems/LINQClass.cs
+++ LINQProblems/LINQClass.cs
@@ -39,6 +39,8 @@ namespace LINQProblems
             var split = grades.Select(g => g.Split(',').Select(s => Convert.ToDouble(s)));
             var split2 = split.Select(s => s.Where(x => x!= s.Min()));
             var split3 = grades.Select(a => a.Split(',').Select(b => Convert.ToDouble(b))).Select(c => c.Where(d => d != c.Min()));
+            var split4 = split3.Select(grades2 => grades2.Average());
+            var split5 = split4.Average();
 
 
             
