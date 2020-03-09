a81a289f36d8f49f84d52ba1821710e52407c2f1
victor-meza-07 <victord.meza@gmail.com>
2020-03-09 16:47:38 -0500

    Problem 3 Progress

--- LINQProblems/LINQClass.cs
+++ LINQProblems/LINQClass.cs
@@ -20,5 +20,33 @@ namespace LINQProblems
         {
             return names.Select(n => n).Distinct().ToList();
         }
+
+        public double ThirdProblem(List<string> grades) 
+        {
+
+            //First Cast to double all of the things in this collection;
+            //for each field, drop the min value, and average the rest;
+            //average the collection;
+
+
+
+            /*
+             SLECT * from ListName
+             where 
+             */
+
+            //var average = grades.Select(g => g.Split(',').Select(s => Convert.ToDouble(s)).Select(o => g.Where(p => p!= g.Min())));
+            var split = grades.Select(g => g.Split(',').Select(s => Convert.ToDouble(s)));
+            var split2 = split.Select(s => s.Where(x => x!= s.Min()));
+            var split3 = grades.Select(a => a.Split(',').Select(b => Convert.ToDouble(b))).Select(c => c.Where(d => d != c.Min()));
+
+
+            
+            
+            
+            Console.ReadLine();
+            
+            return 10.0;
+        }
     }
 }
--- LINQProblems/Program.cs
+++ LINQProblems/Program.cs
@@ -17,6 +17,10 @@ namespace LINQProblems
             {
                 Console.WriteLine(word);
             }
+
+            List<string> vs = new List<string> { "80,100,92,89,65", "93,81,78,84,69", "73,88,83,99,64", "98,100,66,74,55" };
+
+            Console.WriteLine(lINQClass.ThirdProblem(vs));
         }
     }
 }
